1d219fda28c9560547fba8b199e739d4d88b1e70
Wil Pirino <wpirino1@gmail.com>
2016-06-13 20:57:05 -0700

    added resources + resource methods to router

--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -6,19 +6,17 @@ class Route
       pattern, http_method, controller_class, action_name
   end
 
-  # checks if pattern matches path and method matches request method
   def matches?(req)
     method = req.params['_method'] || req.request_method
-    method == http_method.to_s.upcase && req.path =~ pattern
+    method.upcase == http_method.to_s.upcase && req.path =~ pattern
   end
 
   def run(req, res)
-    # use pattern to pull out route params
     match_route = pattern.match(req.path)
     route_params = {}.tap do |params|
       match_route.names.each {|name| params[name] = match_route[name]}
     end
-    # init controller and call action
+
     controller_class.new(req, res, route_params).invoke_action(action_name)
   end
 end
@@ -26,34 +24,75 @@ end
 class Router
   attr_reader :routes
 
+  RESOURCES = [:index, :show, :new, :create, :edit, :update, :destroy];
+  RESOURCE_METHODS = {
+    index: :get,
+    show: :get,
+    new: :get,
+    create: :post,
+    edit: :get,
+    update: :patch,
+    destroy: :delete
+  }
+  RESOURCE_PATTERNS = {
+    index: "^/x$",
+    show: "^/x/(?<id>\\d+)$",
+    new: "^/x/new$",
+    create: "^/x$",
+    edit: "^/x/(?<id>\\d+)/edit$",
+    update: "^/x/(?<id>\\d+)$",
+    destroy: "^/x/(?<id>\\d+)$"
+  }
+
   def initialize
     @routes = []
   end
 
-  # adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
     @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
-  # evaluate the proc in the context of the instance
   def draw(&proc)
     instance_eval(&proc)
   end
 
-  # make each of these methods that
-  # when called adds a route
   [:get, :post, :put, :delete].each do |http_method|
     define_method http_method do |pattern, controller_class, action_name|
       add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
-  # should return the route that matches this request
+  def resources(name, options = {})
+    resources_from_options(options).each do |rsc|
+      pattern = RESOURCE_PATTERNS[rsc].sub("x", name.to_s)
+      controller_class = name.to_s.capitalize + "Controller"
+      add_route(Regexp.new(pattern), RESOURCE_METHODS[rsc], controller_class.constantize, rsc)
+    end
+  end
+
+  def resource(name, options = {})
+    resources_from_options(options).each do |rsc|
+      pattern = RESOURCE_PATTERNS[rsc].sub("x", name.to_s).sub("/(?<id>\\d+)", "")
+      controller_class = name.to_s.pluralize.capitalize + "Controller"
+      add_route(Regexp.new(pattern), RESOURCE_METHODS[rsc], controller_class.constantize, rsc)
+    end
+  end
+
+  def resources_from_options(options)
+    defaults = {only: RESOURCES, except: []}
+    options = defaults.merge(options);
+
+    unless options.values.all? {|rscs| rscs.all? {|rsc| RESOURCES.include?(rsc)}}
+      raise "invalid resource name in resources/resource method on router"
+    end
+
+    options[:only] - options[:except]
+  end
+
   def match(req)
     @routes.find {|route| route.matches?(req)}
   end
 
-  # either throw 404 or call run on a matched route
   def run(req, res)
     route = match(req)
     if route
